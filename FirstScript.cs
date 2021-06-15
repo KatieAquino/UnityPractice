a83538ead0ed5486490e6656ece09935f45fbd53
Hackbright Student <student@hackbrightacademy.com>
2021-06-15 16:15:02 -0700

    Simple Script & Debug Practice for Unity.

--- FirstScript.cs
+++ FirstScript.cs
@@ -11,6 +11,43 @@ public class FirstScript : MonoBehaviour
         string playerName = "Bob";
         float jumpForce = 5.82f;
         bool gameOver = false;
+        float myNumber = 5.0f;
+        myNumber += 15.2f;
+        myNumber *= 3.0f;
+        myNumber--;
+        myNumber++;
+
+        float b = 2.0f;
+        myNumber += b;
+        float c = b * 2.0f;
+
+        Debug.Log(myNumber);
+
+        int numA = 10;
+        int numB = 15;
+
+        if(numA < numB)
+        {
+            Debug.Log("numA is less than numB");
+
+            if(numA == 5 && numB == 6)
+            {
+                Debug.Log("numA is 5 and numb is 6");
+            }
+
+            if(numA > 5 || numB > 10)
+            {
+                Debug.Log("Either numA is greather than 5 or numB is greater than 10.");
+            }
+        }
+        else if(numA > numB)
+        {
+            Debug.Log("numA is greater than numB");
+        }
+        else
+        {
+            Debug.Log("numA is equal to numB");
+        }
     }
 
     // Update is called once per frame
