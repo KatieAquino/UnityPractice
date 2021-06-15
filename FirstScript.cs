3536ecc0314fd34c3accb9665f77b82ac5c89514
Hackbright Student <student@hackbrightacademy.com>
2021-06-15 15:55:50 -0700

    Initial Commit

---
+++ FirstScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int score = 0;
+        string playerName = "Bob";
+        float jumpForce = 5.82f;
+        bool gameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
